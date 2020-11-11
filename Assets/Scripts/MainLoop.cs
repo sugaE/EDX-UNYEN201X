9cabf9ccf04af81ee4de018c1e78bacdf58fe25f
suga <suga_e@outlook.com>
2020-11-11 20:18:23 +0800

    v50

---
+++ Assets/Scripts/MainLoop.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainLoop : MonoBehaviour
+{
+
+    // prefabs
+    public GameObject[] stones = new GameObject[3];
+    // torque force; spin
+    public float torque = 5.0f;
+    // up force 
+    public float minAntiGravity = 20.0f, maxAntiGravity = 40.0f;
+    // l/r force
+    public float minLateralForce = -15.0f, maxLateralForce = 15.0f;
+    public float minTimeBetweenStones = 1f, maxTimeBetweenStones = 3f;
+    public float minX = -30.0f, maxX = 30.0f;
+    public float minZ = -5.0f, maxZ = 20.0f;
+
+    private bool enableStones = true;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(ThrowStones());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator ThrowStones()
+    {
+        // Initial delay
+        yield return new WaitForSeconds(2.0f);
+
+        while (enableStones)
+        {
+
+
+            GameObject stone = (GameObject)Instantiate(stones[Random.Range(0, stones.Length)]);
+            // Y: below the surface; 
+            stone.transform.position = new Vector3(Random.Range(minX, maxX), -30.0f, Random.Range(minZ, maxZ));
+            stone.transform.rotation = Random.rotation;
+
+            rb = stone.GetComponent<Rigidbody>();
+
+            rb.AddTorque(Vector3.up * torque, ForceMode.Impulse); // y
+            rb.AddTorque(Vector3.right * torque, ForceMode.Impulse); // x
+            rb.AddTorque(Vector3.forward * torque, ForceMode.Impulse); // z
+
+            rb.AddForce(Vector3.up * Random.Range(minAntiGravity, maxAntiGravity), ForceMode.Impulse);
+            rb.AddForce(Vector3.right * Random.Range(minLateralForce, maxLateralForce), ForceMode.Impulse);
+
+
+            yield return new WaitForSeconds(Random.Range(minTimeBetweenStones, maxTimeBetweenStones));
+
+        }
+
+    }
+}
+
---
+++ Assets/Scripts/Stone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+
+    //public float impulseForse = 10.0f;
+    public GameObject explosion;
+
+    private const float yOie = -30.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GetComponent<Rigidbody>().AddForce(Vector3.up * impulseForse, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < yOie)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(Instantiate(explosion, transform.position, Quaternion.identity), 2.0f);
+        Destroy(gameObject);
+    }
+}
