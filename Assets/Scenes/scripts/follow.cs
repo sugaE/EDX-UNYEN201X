d8398760b10a437ed2be787c08834d6037c6bd9f
suga <suga_e@outlook.com>
2020-11-11 18:25:08 +0800

    v41

---
+++ Assets/Scenes/scripts/follow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//in camera traking sphere
+public class follow : MonoBehaviour
+{
+    public GameObject target;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.transform.position + offset;
+    }
+}
---
+++ Assets/Scenes/scripts/move.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    public float forceValue;
+    public float jumpValue;
+
+    private Rigidbody rb;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime,
+        //0,
+        //Input.GetAxis("Vertical") * speed * Time.deltaTime);
+
+        //desktop
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < .01f)
+        {
+            rb.AddForce(new Vector3(0, 1, 0) * jumpValue, ForceMode.Impulse);
+
+            audioSource.Play();
+        }
+
+        //mobile
+        if (Input.touchCount == 1)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began && Mathf.Abs(rb.velocity.y) < .01f)
+            {
+                rb.AddForce(new Vector3(0, 1, 0) * jumpValue, ForceMode.Impulse);
+
+                audioSource.Play();
+            }
+
+        }
+        
+
+    }
+
+    //sync with physics engine
+    private void FixedUpdate()
+    {
+        //desktop
+        rb.AddForce(new Vector3(
+            Input.GetAxis("Horizontal"),
+            0,
+            Input.GetAxis("Vertical")) * forceValue);
+
+        //mobile
+        rb.AddForce(new Vector3(
+            -Input.acceleration.y,
+            0,
+            Input.acceleration.x 
+            ) * forceValue);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            print("collision");
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject)
+            print("enter");
+    }
+
+
+}
