f8369324b55c5e2d4d44ce6fc84c6dd5497bb9a4
suga <suga_e@outlook.com>
2020-11-11 18:48:16 +0800

    v42: creating game objects

---
+++ Assets/Scenes/scripts/Impulse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impulse : MonoBehaviour
+{
+
+    public GameObject stone;
+    public float fireRate = .5f;
+
+    private float nextFire = .0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(stone, transform.position, Random.rotation);
+        }
+    }
+}
---
+++ Assets/Scenes/scripts/stone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stone : MonoBehaviour
+{
+
+    public float impulseForse = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * impulseForse, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -1.0f)
+        {
+            Destroy(this);
+        }
+    }
+}
